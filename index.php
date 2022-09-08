60f676f7fd4450dcdd13ca8678f9bf591540c0a0
TatyanaLil <89071506+TatyanaLil@users.noreply.github.com>
2022-09-08 15:06:29 +0500

    меняем имя ссылки

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 						<li class="nav-item">
-							<a class="nav-link active" aria-current="page" href="index.html">Главная</a>
+							<a class="nav-link active" aria-current="page" href="index.html">Самая Главная</a>
 						</li>
 					</ul>
 					<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
